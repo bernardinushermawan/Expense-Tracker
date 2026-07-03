eeba3fadd842bf776f3bc43ff31e939a97d5f986
bernardinushermawan <108281339+bernardinushermawan@users.noreply.github.com>
2026-07-03 15:19:38 +0700

    Update index.ts

--- api/expenses/index.ts
+++ api/expenses/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getSheet } from '../_utils/db';
+import { getSheet } from '../_utils/db.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse)
 {
