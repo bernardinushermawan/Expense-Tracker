08e782db40fa2fc3bac9039cf24edde81d005582
bernardinushermawan <108281339+bernardinushermawan@users.noreply.github.com>
2026-07-03 15:47:31 +0700

    Update db.ts

--- api/_utils/db.ts
+++ api/_utils/db.ts
@@ -1,15 +1,16 @@
-import { GoogleSpreadsheet} from 'google-spreadsheet';
 import { JWT } from 'google-auth-library';
-
-const serviceAccountAuth = new JWT({
-    email: process.env.GOOGLE_CLIENT_EMAIL,
-    key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
 
 const SHEET_ID = '12okkQa5gDIS2R05bvM1yLUIMLUJMDwyXtIlb4diPjk8';
 
 export async function getSheet() {
+  const { GoogleSpreadsheet} = await import('google-spreadsheet');
+
+  const serviceAccountAuth = new JWT({
+    email: process.env.GOOGLE_CLIENT_EMAIL,
+    key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+
   const doc = new GoogleSpreadsheet(SHEET_ID, serviceAccountAuth);
   await doc.loadInfo(); 
   return doc.sheetsByIndex[0]; // Returns sheet1
